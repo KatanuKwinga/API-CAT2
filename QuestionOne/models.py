7e5ceb999b69cbe303689ff3063a49ffa9db7bbe
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-11-29 19:09:41 +0300

    Models for question one

--- QuestionOne/models.py
+++ QuestionOne/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create Customer and Order models.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product
