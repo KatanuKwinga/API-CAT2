98486ad1cd72fe69b2e3ffa3531b6aa532dd3463
KatanuKwinga <kavila.katanu@strathmore.edu>
2024-11-29 14:55:57 +0300

    Views, Serializers and urls

--- QuestionTwo/QuestionTwo/products/views.py
+++ QuestionTwo/QuestionTwo/products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Building API Views
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListCreateView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
